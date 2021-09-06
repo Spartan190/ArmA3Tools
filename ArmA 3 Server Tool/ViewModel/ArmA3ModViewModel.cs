a90a18e43cb070541137f0d3f1b60b340b74ef23
Sven Steinbach <ste-sven@gmx.de>
2021-09-06 19:59:32 +0200

    resolved merge conflict of fix and getting of stashed

---
+++ ArmA 3 Server Tool/ViewModel/ArmA3ModViewModel.cs
@@ -0,0 +1,48 @@
+﻿using ArmA3PresetList;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmA_3_Server_Tool.ViewModel
+{
+    public class ArmA3ModViewModel : ArmA3Mod, INotifyPropertyChanged
+    {
+
+        public override string DisplayName { 
+            get => base.DisplayName; 
+            protected set {
+                base.DisplayName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DisplayName"));
+            }
+        }
+
+        public override string Link { 
+            get => base.Link;
+            protected set {
+                base.Link = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Link"));
+            }
+        }
+
+        public override string WorkshopId { 
+            get => base.WorkshopId;
+            protected set {
+                base.WorkshopId = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("WorkshopId"));
+            }
+        }
+
+        public ArmA3ModViewModel(string displayName, string link, string workshopId) : base(displayName, link, workshopId)
+        {
+        }
+
+        public ArmA3ModViewModel(ArmA3Mod mod) : this(mod.DisplayName, mod.Link, mod.WorkshopId)
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
